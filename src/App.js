67873577c72181f1ac86831c7436196f12be99db
rudcjf0399 <rudcjf0399@gmail.com>
2024-10-17 23:44:03 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// import {
+//     MainButton,
+//     useShowPopup,
+//     WebAppProvider,
+// } from "@vkruglikov/react-telegram-web-app";
+
+// const App = () => {
+//     const showPopup = useShowPopup();
+
+//     const handleClick = () =>
+//         showPopup({
+//             message: "Hello, I am popup",
+//         });
+
+//     return (
+//         <WebAppProvider
+//             options={{
+//                 smoothButtonsTransition: true,
+//             }}
+//         >
+//             <MainButton text="SHOW POPUP" onClick={handleClick} />
+//         </WebAppProvider>
+//     );
+// };
+
+// export default App;
+
+const App = () => {
+    return <h1>GitHub Pages로 리액트 프로젝트 배포</h1>;
+};
+
+export default App;
